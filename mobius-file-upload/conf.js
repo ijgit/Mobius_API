aa691c565747e907744e283bb81a7b538ad248c5
ijgit <isuu.jeong@gmail.com>
2021-11-22 11:33:56 +0900

    add mobius file upload server

---
+++ mobius-file-upload/conf.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ip: `192.168.50.157`,   // mongodb server ip addr
+    port: 3000,
+
+    // mobius
+    mp_url: '203.253.128.161:7579/Mobius',  // mobius ip
+    ae: 'jeong'
+}
